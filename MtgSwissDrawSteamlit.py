37cfe9e69d7b37290ff4df1fc96e7cd4498a4310
MasayukiTamate <masayuki.tamate.anago@gmail.com>
2025-08-20 11:48:10 +0900

    成績表示１

--- MtgSwissDrawSteamlit.py
+++ MtgSwissDrawSteamlit.py
@@ -2,10 +2,24 @@ import streamlit as st
 
 playerName = []
 playerNameKari = []
+playerData = []
+
+def addplayer(n):
+    playerData.append({
+        "名前": n,
+        "勝ち点": 0,
+        "マッチポイント％":0.000,
+        "オポメント％":0.000
+    })
+    pass
+def taisenKime():
+    pass
 
 def SeisekiHyouji(itu):
     text = itu + "成績"
     st.write(text)
+    for i in playerData:
+        st.write(f"名前:{i["名前"]} 勝ち点:{i["勝ち点"]} MP%:{i["マッチポイント％"]} OP%:{i["オポメント％"]}")
     pass
 
 
@@ -28,15 +42,21 @@ if st.button("プレイヤーの削除", key=98):
         st.session_state["count"] -= 1
 for i in range(st.session_state["count"]):
     playerName.append( st.text_input("プレイヤー", i,key=i) )
-
-st.write(playerName)
-
-
-st.button("一回戦")
+    addplayer(i)
 
 
-SeisekiHyouji("最終")
+
+for i, j in zip(playerName, playerData):
+    j["名前"] = i
+
+#st.button("一回戦")
+
+taisenKime()
+
+
 SeisekiHyouji("一回戦終了時")
 SeisekiHyouji("最終")
+
+
 
 #magic mpc
