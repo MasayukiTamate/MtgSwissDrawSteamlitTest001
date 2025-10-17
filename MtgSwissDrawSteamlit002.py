48000321510fb5f2dc0b3f259ba061f0d47f90c0
masayuki.tamate <masayuki.tamate.anago@gmail.com>
2025-10-17 12:34:57 +0900

    最初のテキストボックス設置１

--- MtgSwissDrawSteamlit002.py
+++ MtgSwissDrawSteamlit002.py
@@ -7,6 +7,16 @@ MTGスイスドロー大会の成績管理アプリ v0.002
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+class PlayerData():
+    def __init__(self, na):
+        self.name = na
+        self.winPoint = 0
+        self.matchPointPar = 0.00
+        self.opponentPar = 0.00
+        self.opponentHistory = []
+        pass
+
+
 #フラグとカウンター
 #人数をグローバルに
 if 'count' not in st.session_state:
@@ -19,7 +29,15 @@ topplayertuikabotton = st.columns(2)
 
 if topplayertuikabotton[0].button("プレイヤーの追加", key=99):
     st.session_state["count"] += 1
+beforName = ["たなか","やまだ","さかもと"]
 
-'''
-表示されるコメント
-'''
+pData = []
+
+for beNe in beforName:
+    pData.append(PlayerData(beNe))
+    
+#テキストボックス設置
+count = 1
+for pd in pData:
+    st.text_input("プレイヤー", pd.name,key=count + 100)
+    count = count + 1
