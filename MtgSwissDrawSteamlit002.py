ea05fbef3e5bde2472d52967709bed59a78e3720
masayuki.tamate <masayuki.tamate.anago@gmail.com>
2025-10-17 13:33:44 +0900

    試合の席毎の名前を表記１

--- MtgSwissDrawSteamlit002.py
+++ MtgSwissDrawSteamlit002.py
@@ -16,6 +16,18 @@ class PlayerData():
         self.opponentHistory = []
         pass
 
+class SeatData():
+    def __init__(self, pd):
+
+        
+        count = int(len(pd) // 2)
+        if len(pd) % 2:
+            count = count + 1
+        self.CountSeat = count
+        count = count - 1
+        pass
+
+
 beforName = ["たなか","やまだ","さかもと"]
 pData = []
 
@@ -54,8 +66,31 @@ for i in range(st.session_state["count"]):
     st.text_input("プレイヤー", pData[i].name ,key=count + 100)
     count = count + 1
 
+tentative = []
 
 
+
+st.write("試合１")
+sData = SeatData(pData)
+
+count = 0
+for couS in range(sData.CountSeat):
+    st.write("席" + str(couS + 1) + "つ目")
+    st.write(f"{pData[count].name}")
+    
+    count = count + 1
+    if count < len(pData):
+        st.write(f"{pData[count].name}")
+        count = count + 1
+
+st.write("試合１の成績")
+tentative.append(pData.copy())
+for pd in tentative[0]:
+    st.write(f"{pd.name} 勝利点：{pd.winPoint} ウィンP％：{pd.matchPointPar} オポP％：{pd.opponentPar} 敵履歴；{pd.opponentHistory}")
+st.write("試合２")
+st.write("試合３")
+pData[0].name = "ありよし"
+st.write("最終成績")
 for pd in pData:
     st.write(f"{pd.name} 勝利点：{pd.winPoint} ウィンP％：{pd.matchPointPar} オポP％：{pd.opponentPar} 敵履歴；{pd.opponentHistory}")
 '''
